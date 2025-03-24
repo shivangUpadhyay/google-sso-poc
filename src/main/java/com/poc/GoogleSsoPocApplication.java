2682a523abf2763688c963418889bdce8dad6c7e
Shivang Upadhyay <shivang.upadhyay@enveu.com>
2025-03-24 13:00:11 +0530

    initial commit: Google SSO POC.

---
+++ src/main/java/com/poc/GoogleSsoPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSsoPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleSsoPocApplication.class, args);
+
+    }
+}
